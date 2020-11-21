de91666e13a1cf35b34d1b9cd39fe5fb6148502e
geshengming <geshengming17@163.com>
2020-11-21 21:54:15 +0800

    modify-address

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0x4a432E955fE91f13e4908aa4de697498D12c4FED'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
